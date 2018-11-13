ccd8c8921dd9ea6895d03757a781e7a15487b640
Cornelis Seevinck <corky@seevinck.com>
2018-11-13 06:33:30 -0700

    Add rough code for most fuctionality
    Feat : Add code to prove the concept.
    
    - Add event listeners
    
    - Flip cards
    
    - If no match - hide cards
    
    - If match - wait for more clicks
    
    - Disable clicks while waiting for cards hide
    
    - Lots of debug code in here
    
    Need to see if this can be simplified - maybe an event handler approach

--- js/app.js
+++ js/app.js
@@ -25,6 +25,74 @@ function shuffle(array) {
     return array;
 }
 
+// Code for the program
+// Set listener events on all cards
+let allCards = document.querySelectorAll('.card');
+let clickCount = 0;
+let matchCount = 0;
+let firstIndex;
+let allowClicks = true; // allow clicks - will be turned off during timeout
+allCards.forEach(function(card, cardIndex, cardArray) {
+	card.addEventListener ('click', function (e) {
+    if (allowClicks) {
+      console.log ('Click here - clickCount = ' + clickCount + '  cardIndex = ' + cardIndex);
+//  process click if the card is not already visible
+      if (!(card.classList.contains('show'))) {  //
+        switch (clickCount++) {
+// first click event - show card
+        case 0:
+          card.classList.add('open', 'show');
+          console.log ('first click');
+          firstIndex = cardIndex;
+          console.log ('Case 0 clickCount = ' + clickCount + '  firstIndex = ' + cardIndex);
+          break;
+// second click event - show card & look if match with first card
+        case 1:
+          card.classList.add('open', 'show');
+          console.log ('second click - clickCount = ' + clickCount);
+
+          // here to check for a match
+          if (CardsMatch (cardArray [firstIndex], card)){
+            console.log ('cards match - clickCount = ' + clickCount);
+
+            // here to check for all cards match
+            if (matchCount++ > 15) {
+              // we are done - you are a winner
+              console.log ('All cards match - do end');
+              alert ("You won - good job")
+            }
+            else {
+              clickCount = 0; // prepare for another match
+            }
+          }
+          else {
+            console.log ('no match  - clickCount = ' + clickCount);
+// wait for 2 seconds then hide the 2 cards
+            allowClicks = false; // setting to ignore clicks till after timeout
+            setTimeout(function(){
+              cardArray [firstIndex].classList.remove('open', 'show'); // hide first card
+              card.classList.remove('open', 'show'); // hide second card
+              clickCount = 0;  // prepare for another match
+              allowClicks = true; // setting to allow clicks again
+              console.log ('Allow clicks again - clickCount = ' + clickCount);
+              }, 2000);
+            }
+          break;
+        default:
+          alert ('This is a bug');
+          console.log ('beyond second click - should not her here!');
+        }; // end of switch
+      }
+      else {
+        // alert ('Allready visible');
+        console.log ('Allready visible - do nothing - clickCount = ' + clickCount);
+      };
+    }
+    else {
+      console.log ('Don"t allow Clicks - clickCount = ' + clickCount);
+    };
+  });
+});
 
 /*
  * Check if the two supplied cards match
